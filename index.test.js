bbee5590d157edd0310366ab59afd1310ba6597d
danielstaleiny <daniel.rafaj@gmail.com>
2017-11-28 21:53:13 +0100

    add test, snyk, nodemon and package scripts

---
+++ index.test.js
@@ -0,0 +1,8 @@
+const fetchData = () => {
+  return new Promise((resolve, reject) => resolve("success"));
+};
+
+test("the test", async () => {
+  const data = await fetchData();
+  expect(data).toBe("success");
+});
