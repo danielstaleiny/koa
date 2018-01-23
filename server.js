1ec87e533c159fafa488881a003fbdbb7605a575
danielstaleiny <daniel.rafaj@gmail.com>
2018-01-23 20:27:43 +0100

    add generic methods for get by id and post,

--- server.js
+++ server.js
@@ -27,27 +27,88 @@ const User = require("./user.js");
 // id is your local variable you can call it whatever
 // router.param('user', (id, ctx, next))
 
-router
-    .param("user", async (id, ctx, next) => {
-        // console.log("id ", id);
-        // finds by _id
-        // let user = await User.findById(id);
+// returns string with first capital letter
+// param: string -> string
+const toUpperCaseFirst = str => `${str.charAt().toUpperCase()}${str.substr(1)}`;
 
-        let user = new User({ id, text: "ni8ceee" });
-        user = await user.save();
-        // let user = await User.findOne({ id });
-        // if (!user) return ctx.throw(404, "User id not found");
-
-        console.log(user);
-        // await user.save();
-        ctx.user = user;
+// generates param for model
+// generates param for searchBy attribute
+// example: parseParam('id')('user')
+// example: searchBy = 'id' && model= 'user'
+// example: returns function which search by User.findOne({id: value})
+// ctx.user = item
+// and set it to model value on ctx
+const parseParam = searchBy => model => async (value, ctx, next) => {
+    // take schema model
+    const Model = require(`./${model}`);
+    var obj = {};
+    // specify what to search by
+    // example: {id: value}
+    obj[searchBy] = value;
+    try {
+        let item = await Model.findOne(obj);
+        // make sure to return 404 if none was found
+        if (!item)
+            return ctx.throw(404, `${toUpperCaseFirst(model)} not found`);
+        ctx[model] = item;
         return next();
-    })
-    .get("/users/:user", ctx => {
-        User.getById();
-        ctx.status = 200;
-        ctx.body = ctx.user;
-    })
+    } catch (e) {
+        return ctx.throw(500, e);
+    }
+};
+
+const compose = (...fns) => {
+    return result => {
+        const list = fns.slice();
+
+        while (list.length > 0) {
+            result = list.pop()(result);
+        }
+
+        return result;
+    };
+};
+
+const pipe = (...fns) => {
+    return compose(...fns.reverse());
+};
+
+const injectModel = model => (ctx, next) => {
+    ctx[model] = require(`./${model.toLowerCase()}`);
+    return next();
+};
+
+const create = model => async ctx => {
+    // create user with body
+    const item = new ctx[model](ctx.request.body);
+    try {
+        const savedItem = await item.save();
+        ctx.status = 201;
+        ctx.body = savedItem;
+    } catch (e) {
+        return ctx.throw(500, e);
+    }
+};
+
+const get = model => ctx => {
+    ctx.status = 200;
+    ctx.body = ctx[model];
+};
+
+const userControllerGet = get("user");
+// const userControllerPost =  create("User");
+const parseById = parseParam("id");
+const userParam = parseById("user");
+
+router
+    .param("user", userParam)
+    .post("/users", jsonParser, injectModel("User"), create("User"))
+    .get("/users/:user", userControllerGet)
+    // .get("/users/:user", ctx => {
+    // User.getById();
+    // ctx.status = 200;
+    // ctx.body = ctx.user;
+    // })
     .get("/", ctx => {
         ctx.throw(400, "waaa");
     })
